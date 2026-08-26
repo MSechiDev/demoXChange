a1a2d119ed5e713dea53a24957b5c54167854fa1
KetchOxy <manuelsechi330@gmail.com>
2026-08-26 14:33:43 +0200

    mapped all the entities with hibernate

---
+++ src/main/java/org/generation/italy/demoxchange/model/repositories/ItemImageRepository.java
@@ -0,0 +1,7 @@
+package org.generation.italy.demoxchange.model.repositories;
+
+import org.generation.italy.demoxchange.model.entities.ItemImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
+}
---
+++ src/main/java/org/generation/italy/demoxchange/model/repositories/OfferRepository.java
@@ -0,0 +1,7 @@
+package org.generation.italy.demoxchange.model.repositories;
+
+import org.generation.italy.demoxchange.model.entities.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+}
